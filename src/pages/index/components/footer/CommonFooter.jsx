73c5a96ddc22b25daa6b56f8d9aa360e1ca44f75
leeyujin0527 <sksdldbwls0527@gmail.com>
2025-06-16 09:30:52 +0900

    푸터 페이지 값

--- src/pages/index/components/footer/CommonFooter.jsx
+++ src/pages/index/components/footer/CommonFooter.jsx
@@ -1,16 +1,32 @@
 import styles from './CommonFooter.module.scss';
 import right from '@/assets/icons/icon-arrowRight.svg';
 import left from '@/assets/icons/icon-arrowLeft.svg';
+import { pageState } from '../../../../recoil/atoms/pageState';
+import { useRecoilState } from 'recoil';
+
 
 function CommonFooter() {
+  const [page,setPage] = useRecoilState(pageState)
+  const leftBtn = () =>{
+    if(page<=1){
+      setPage(1)
+    }else{
+      setPage(page-1)
+    }
+  }
+
+  const rightBtn = () =>{
+    setPage(page+1);
+  }
+
   return (
     <footer className={styles.footer}>
       <div className={styles.pagination}>
-        <button className={styles.pagination__button}>
+        <button className={styles.pagination__button} onClick={leftBtn}>
           <img src={left} alt="이전" />
         </button>
-        <span>1</span>
-        <button className={styles.pagination__button}>
+        <span>{page}</span>
+        <button className={styles.pagination__button} onClick={rightBtn}>
           <img src={right} alt="다음" />
         </button>
       </div>
